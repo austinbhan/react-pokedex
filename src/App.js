0c5359defc8dddbbf85c7912addeb45999036759
austinbhan <austinbhan@outlook.com>
2022-06-29 11:54:23 -0700

    PokemonList, pokemon.js, created and App.js imported

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 
+import PokemonList from './PokemonList';
+
 function App() {
   return (
     <div className="App">
-
+      <h1>This is the front page</h1>
+      <div className="pokemon">
+        <PokemonList />
+      </div>
     </div>
   );
 }
---
+++ src/PokemonList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function PokemonList() {
+  return (
+    <h2>PokemonList has been successfully imported</h2>
+  );
+}
