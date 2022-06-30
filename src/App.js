8c49f7d3eb18691ec79b78524a25ff6d574005ae
austinbhan <austinbhan@outlook.com>
2022-06-29 17:55:23 -0700

    CSS lists

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 import PokemonSearch from './PokemonSearch';
+import YelpSearch from './YelpSearch';
 
 function App() {
 
 
   return (
-    <div className="App">
-      <h1>This is the front page</h1>
-      <div className="pokemon-list">
-        <PokemonSearch />
+    <>
+      <h1 id="title">This is the front page</h1>
+      <div className="App">
+        <div className="pokemon-list">
+          <PokemonSearch />
+        </div>
+        <div className="yelp-list">
+          <YelpSearch />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/YelpSearch.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function YelpSearch() {
+  return (
+    <div>Yelp Search has successfully been imported</div>
+  );
+}
